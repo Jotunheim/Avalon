b79a0b6fa67052b50fdba868bcf33a27973a517a
Ut Duong <utduong1989@yahoo.com>
2011-09-18 19:22:35 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avalon::Application.config.secret_token = '0dfc96069c851d73e4c5eeacbfebdef18ca6711122905c3ab985558d08b807100e7ee97a0a80306b8cd2183af00cc23c4ea731a3c5a2af823f4a910b88fb9c5c'
